5f1a1f5899693ab2f0b55e8bd7f080713f51699f
BrightBlack <mapindurt@gmail.com>
2024-11-27 14:56:57 +0200

    day range

--- app/Controllers/Premium.php
+++ app/Controllers/Premium.php
@@ -29,10 +29,35 @@ class Premium extends BaseController
         $holder_relationships = $this->holderRelationships();
         $calendar_months = $this->calendarMonths();
         $year_range = $this->yearRange();
+        $calendar_days = $this->calendarDays();
 
-        log_message('debug', 'Data passed to view: ' . json_encode($year_range));die;
+        log_message('debug', 'Data passed to view: ' . json_encode($calendar_days));die;
         
         // return view('wsdl_view', ['data' => $marital_statuses]);
+    }
+    public function calendarDays()
+    {
+        try {
+            $client = new SoapClient($this->wsdl, $this->options);
+            $response = $client->    GetCalendarDayRangeItems();
+            $responseArray = json_decode(json_encode($response), true);
+            // echo "<pre>", print_r($responseArray); die();
+            if (isset($responseArray['GetCalendarDayRangeItemsResult']['any'])) {
+                    $anyString = $responseArray['GetCalendarDayRangeItemsResult']['any'];
+                    $anyString = '<root>' . $anyString . '</root>';
+                    $associativeArray = $this->processDays($anyString);
+                    if (!empty($associativeArray)) {
+                        return $associativeArray;
+                    } else {
+                        return ['error' => 'Processed array is empty.'];
+                    }
+                } else {
+                    return ['error' => 'Expected keys not found in the response.'];
+                }
+
+        } catch (Exception $e) {
+            return view('error_view', ['error' => $e->getMessage()]);
+        }
     }
     public function yearRange()
     {
@@ -276,6 +301,29 @@ class Premium extends BaseController
      * @param string $anyString
      * @return array
      */
+     private function processDays($anyString) {
+        // log_message('debug', 'Raw XML content: ' . $anyString);die;
+    $anyString = '<root>' . $anyString . '</root>';
+    libxml_use_internal_errors(true);
+    $xml = simplexml_load_string($anyString);
+    if ($xml === false) {
+        $errors = libxml_get_errors();
+        foreach ($errors as $error) {
+            log_message('debug', 'XML Error: ' . $error->message);
+        }
+        return [];
+    }
+    $xml->registerXPathNamespace('diffgr', 'urn:schemas-microsoft-com:xml-diffgram-v1');
+    $day_range = $xml->xpath('//diffgr:diffgram/DocumentElement/DayRange');
+    $array = [];
+    foreach ($day_range as $range) {
+        $array[] = [
+            'Value' => (string) $range->Value,
+            'Description' => (string) $range->Description,
+        ];
+    }
+    return $array;
+}
     private function processRange($anyString) {
         // log_message('debug', 'Raw XML content: ' . $anyString);die;
     $anyString = '<root>' . $anyString . '</root>';
